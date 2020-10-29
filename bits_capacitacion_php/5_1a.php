4552c496db0ade8f1af28eda0af4645e8134d0ec
Nicholas Silva Ruiz <brandon.silva@bitsamericas.com>
2020-10-29 16:19:25 -0500

    Solución ejercicio 5_1a.php

---
+++ bits_capacitacion_php/5_1a.php
@@ -0,0 +1,14 @@
+<?php
+
+    echo "Distintas operaciones entre dos numeros: <br><br>";
+
+    $num1 = 31;
+    $num2 = 98;
+
+    echo "La Suma de " . $num1 . " + " . $num2 . " es igual a: " . $suma = ($num1 + $num2) . "<br>";
+    echo "La Resta de " . $num1 . " - " . $num2 . " es igual a: " . $resta = ($num1 - $num2) . "<br>";
+    echo "La Multiplicacion de " . $num1 . " * " . $num2 . " es igual a: " . $mult = ($num1 * $num2) . "<br>";
+    echo "La Division de " . $num1 . " / " . $num2 . " es igual a: " . $division = ($num1 / $num2) . "<br>";
+    echo "El resto de la division de " . $num1 . " % " . $num2 . " es igual a: " . $resto = ($num1 % $num2) . "<br>";
+
+?>
