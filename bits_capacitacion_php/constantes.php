a9623e11b62ff91b4d63ec285bb3656afc210258
Nicholas Silva Ruiz <brandon.silva@bitsamericas.com>
2020-10-29 16:14:26 -0500

    Definir constantes

---
+++ bits_capacitacion_php/constantes.php
@@ -0,0 +1,13 @@
+<?php
+
+    define("DESCUENTO", 10); // se hace descuento de 10€
+
+    $precio_artic1 = 50;
+    $precio_artic2 = 43.5;
+
+    echo "Precio inicial articulo 1 es: " . $precio_artic1;
+    echo "<p>El articulo 1 con descuento es: " . ($precio_artic1 - DESCUENTO) . " €.</p>";
+
+    echo "Precio inicial articulo 2 es: " . $precio_artic2;
+    echo "<p>El articulo 2 con descuento es: " . ($precio_artic2 - DESCUENTO) . " €.</p>";
+?>
