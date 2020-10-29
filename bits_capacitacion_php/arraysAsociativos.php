e20107110ff92d9ac43276873c506dbb4332281c
Nicholas Silva Ruiz <brandon.silva@bitsamericas.com>
2020-10-29 16:19:04 -0500

    Arrays Asociativos

---
+++ bits_capacitacion_php/arraysAsociativos.php
@@ -0,0 +1,33 @@
+<?php
+
+    //---------- Asignacion elemento a elemento
+    $prefijo ["Alemania"] = 49;
+    $prefijo ["Argentina"] = 54;
+    $prefijo ["Colombia"] = 57;
+    $prefijo ["España"] = 34;
+    $prefijo ["Mexico"] = 52;
+
+
+    //-------------- Asignacion clasica corta 
+
+    $prefijo = array (
+        "Alemania" => 49,
+        "Argentina" => 54,
+        "Colombia" => 57,
+        "España" => 34,
+        "Mexico" => 52
+    );
+
+    //-------------- Asignacion formato nuevo
+
+    $prefijo = [
+        "Alemania" => 49,
+        "Argentina" => 54,
+        "Colombia" => 57,
+        "España" => 34,
+        "Mexico" => 52
+    ];
+
+    echo "El prefijo para llamar a Alemania es " . $prefijo["Alemania"] . "<br>";
+    echo "El prefijo para llamar a Colombia es " . $prefijo["Colombia"] . "<br>";
+?>
