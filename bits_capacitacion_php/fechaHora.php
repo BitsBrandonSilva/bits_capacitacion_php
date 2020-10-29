344870797d23fd31d52e08d140ea18d43d9ffb56
Nicholas Silva Ruiz <brandon.silva@bitsamericas.com>
2020-10-29 16:10:49 -0500

    Mostrar fecha y hora

---
+++ bits_capacitacion_php/fechaHora.php
@@ -0,0 +1,19 @@
+<?php
+    $fecha = date ("d/m/y");
+    $hora = date ("h:i:s");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fecha y hora</title>
+</head>
+<body>
+
+    <p><?php echo "Fecha:" .$fecha; ?></p>
+    <p><?php echo "Hora:" .$hora; ?></p>
+    
+</body>
+</html>
