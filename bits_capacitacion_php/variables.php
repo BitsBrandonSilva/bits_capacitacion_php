70af9e514d6eda0963a6897872a2e22f963bf638
Nicholas Silva Ruiz <brandon.silva@bitsamericas.com>
2020-10-29 16:08:28 -0500

    asignación de variables

---
+++ bits_capacitacion_php/variables.php
@@ -0,0 +1,10 @@
+<?php
+    //-------------Asignacion de variables numeros y cadenas de textos
+    $numero = 31;
+    $nombre = "Nicholas";
+    $apellido = "Silva";
+
+    echo "<p>El valor de la variable numero es: $numero.</p>";
+    echo "<p>El valor de la variable nombre es: $nombre.</p>";
+    echo "<p>El valor de la variable apellido es: $apellido.</p>";
+?>
