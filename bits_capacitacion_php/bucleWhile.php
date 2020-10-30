710415d1fd97ed139057b96c8ee07c9712f396d6
Nicholas Silva Ruiz <brandon.silva@bitsamericas.com>
2020-10-30 10:36:08 -0500

    Estructura bucle While

---
+++ bits_capacitacion_php/bucleWhile.php
@@ -0,0 +1,24 @@
+<?php
+
+    $vector = [40, 31, 22, 10, 5];
+
+    $suma_total = 0;
+
+    $v = 0;
+
+    //----------------- bucle while que muestra el vector
+    while($v < count($vector)) {
+        echo $vector[$v] . "<br>";
+        $v++;
+    }
+    echo "<br>";
+    $i = 0;
+
+    //--------------------- bucle while que cuenta y suma el vector
+    while ($i < count($vector)) {
+        $suma_total = $suma_total + $vector[$i];
+        $i++;
+    }
+
+    echo "La suma total del vector es: " . $suma_total;
+?>
