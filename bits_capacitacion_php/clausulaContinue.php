c3cdea34e34d0145f9b3fe4a483abe63b9be711c
Nicholas Silva Ruiz <brandon.silva@bitsamericas.com>
2020-10-30 15:00:31 -0500

    Clausula continue

---
+++ bits_capacitacion_php/clausulaContinue.php
@@ -0,0 +1,19 @@
+<?php
+
+    echo "Se salta los numeros pares a causa de la clausula continue <br>";
+    for ($i = 0; $i < 10; $i++ ) {
+        if ($i % 2 == 0) {
+            continue;
+        }
+        print "$i <br>";
+    }
+    echo "<br>";
+
+    echo "Se salta los numeros multiplos de 3 <br>";
+    for ($i = 0; $i < 10; $i++ ) {
+        if ($i % 3 == 0) {
+            continue;
+        }
+        print "$i <br>";
+    }
+?>
