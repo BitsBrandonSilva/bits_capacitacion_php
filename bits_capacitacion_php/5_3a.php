7e293090d8ecc8b4d7ea2f8934333c7d6e6848ea
Nicholas Silva Ruiz <brandon.silva@bitsamericas.com>
2020-10-30 18:22:25 -0500

    Correccion ejercicio 5_3a.php

--- bits_capacitacion_php/5_3a.php
+++ bits_capacitacion_php/5_3a.php
@@ -35,18 +35,44 @@
         ]
     ];
 
-    //$numero_peliculas = sizeof($peliculas);
-
-    //-----------------Estructura de repeticion que cuenta las peliculas
+    //-----------------Estructura Ciclo Foreach que cuenta las peliculas
     $cont = 0;
     foreach($peliculas as $movie) {
         $cont = $cont + 1;
     }
-    echo "El numero de peliculas existentes es: " . $cont . "<br><br>";
 
-    echo "Las peliculas son: <br><br>";
+    echo "Cantidad de peliculas con Ciclo Foreach: " . $cont . "<br><br>";
+
+    $longitud = count($peliculas);
+
+    //--------------------------Estructura Ciclo While
+    $i = 0;
+    while ($i < $longitud) {
+        $i++;
+    }
+
+    echo "Cantidad de peliculas con Ciclo While: $i <br><br>";
+
+    //-------------------Estructura Ciclo DoWhile
+
+    $p=0;
+    do {
+        $p++;
+    } while ($p < $longitud);
+    
+    echo "Cantidad de peliculas con Ciclo DoWhile: $p <br><br>";
+
+    //-------------------Estructura Ciclo For
+
+    $cont2 = 0;
+    for ($i = 0; $i < $longitud; $i++) {
+        $cont2++;
+    }
+
+    echo "Cantidad de peliculas con Ciclo For: $cont2 <br><br>";
 
     //--------------------Estructura de repeticion que muestra las peliculas
+    echo "Las peliculas son: <br><br>";
     foreach($peliculas as $movie) {
         echo $movie["titulo"] . "<br>";
     }
