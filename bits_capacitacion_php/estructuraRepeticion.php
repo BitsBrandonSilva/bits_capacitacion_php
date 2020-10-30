a8845b1cbc4d61ab90e0fe8cedabd27d17298861
Nicholas Silva Ruiz <brandon.silva@bitsamericas.com>
2020-10-30 10:13:49 -0500

    Estructura de repeticion for

---
+++ bits_capacitacion_php/estructuraRepeticion.php
@@ -0,0 +1,26 @@
+<?php
+
+    echo "Bucle for con longitud fija: <br><br>";
+    for ($i = 0; $i<10; $i++) {
+        echo $i . "<br>";
+    }
+    echo "<br>";
+
+    echo "Bucle for que su limite varia segun la longitud de un array: <br><br>";
+    $vector = [5, 10, 22, 31, 40];
+
+    $suma_total = 0;
+
+    for ($i = 0; $i < count ($vector); $i++) {
+        echo $vector[$i] . "<br>";
+    }
+    echo "<br>";
+
+    for ($i = 0; $i < count ($vector); $i++) {
+        $suma_total = $suma_total + $vector[$i];
+    }
+
+    echo "Suma del contenido del array: <br>";
+    echo $suma_total;
+
+?>
