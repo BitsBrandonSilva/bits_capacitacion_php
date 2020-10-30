72f784811895f4fd7a8012bb6efa40d8d3387ff7
Nicholas Silva Ruiz <brandon.silva@bitsamericas.com>
2020-10-30 13:05:14 -0500

    Condicional ternario

---
+++ bits_capacitacion_php/condicionalTernario.php
@@ -0,0 +1,12 @@
+<?php
+
+    $a = 10;
+    $b = 25;
+    $c = 30;
+
+    $a > $b ? print "A" : print "B <br>";
+
+    $b == $c ? print "B = C" : print "B != C <br>";
+
+    ($c > $b || $b < $a) ? print "SI" : print "NO";
+?>
