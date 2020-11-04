3418aaef2eace164b073682b299a5e2b898bf3b6
Nicholas Silva Ruiz <brandon.silva@bitsamericas.com>
2020-11-04 12:07:11 -0500

    Correccion ejercicio 5_3d.php

--- bits_capacitacion_php/5_3d.php
+++ bits_capacitacion_php/5_3d.php
@@ -26,12 +26,10 @@
         if ($empleados["Salary"] < 1000000) {
             echo $empleados["Firstname"] . " tiene salario bajo <br>";
             echo $empleados["Salary"] . "<br>";
-        }
-        if ($empleados["Salary"] > 2000000) {
+        }elseif ($empleados["Salary"] > 2000000) {
             echo $empleados["Firstname"] . " tiene salario alto <br>";
             echo $empleados["Salary"] . "<br>";
-        }
-        if ($empleados["Salary"] > 1000000 && $empleados["Salary"] < 2000000) {
+        }elseif ($empleados["Salary"] > 1000000 && $empleados["Salary"] < 2000000) {
             echo $empleados["Firstname"] . " tiene salario medio <br>";
             echo $empleados["Salary"] . "<br>";
         }
@@ -42,24 +40,18 @@
     //------------------------------------------Rangos de salario por persona
     if ($employees[13]["Salary"] < 1000000) {
         echo $employees[13]["Firstname"] . " " . $employees[13]["Lastname"] . " con ID: " . key($employees) . ", tiene un salario bajo <br>";
-    }
-
-    if ($employees[3]["Salary"] > 2000000) {
-        echo $employees[3]["Firstname"] . " " . $employees[3]["Lastname"] . " con ID: " . "3" . ", tiene un salario alto <br>";
-    }
-
-    if ($employees[42]["Salary"] > 1000000 || $employees[42]["Salary"] < 2000000) {
-        echo $employees[42]["Firstname"] . " " . $employees[3]["Lastname"] . " con ID: " . "42" . ", tiene un salario medio <br><br>";
+        if ($employees[3]["Salary"] > 2000000) {
+            echo $employees[3]["Firstname"] . " " . $employees[3]["Lastname"] . " con ID: " . "3" . ", tiene un salario alto <br>";
+            if ($employees[42]["Salary"] > 1000000 || $employees[42]["Salary"] < 2000000) {
+                echo $employees[42]["Firstname"] . " " . $employees[42]["Lastname"] . " con ID: " . "42" . ", tiene un salario medio <br><br>";
+            }
+        }
     }
 
 
     //--------------------- Rango mas alto
-
-    foreach($employees as $empleados) {
-        if ($empleados["Salary"] > 2000000) {
-            echo $empleados["Firstname"] . " tiene salario mas alto de todos<br>";
-            echo $empleados["Salary"] . "<br>";
-        }
+    if ($employees[3]["Salary"] > 2000000) {
+        echo $employees[3]["Firstname"] . " tiene el salario mas alto de todos<br>";
+        echo $employees[3]["Salary"] . "<br>";
     }
-
 ?>
