3c273f51038957f44bce01fbfb9ae5a1bc6d9489
Nicholas Silva Ruiz <brandon.silva@bitsamericas.com>
2020-11-05 12:53:39 -0500

    Solución ejercicio5_4b.php

---
+++ bits_capacitacion_php/5_4b.php
@@ -0,0 +1,31 @@
+<?php
+
+    include '5_4a.php';
+
+   $numeros = [
+      10 => [
+            "numero" => rand(1, 900)
+      ],
+
+      20 => [
+            "numero" => rand(1, 900)
+      ],
+
+      30 => [
+          "suma" => '+',
+          "resta" => '-',
+          "multiplicacion" => '*',
+          "division" => '/',
+          "resto" => '%' 
+      ]
+   ];
+   
+    while ($numeros) {
+        echo "Suma: " . calculadora($numeros[10]["numero"], $numeros[20]["numero"], $numeros[30]["suma"]) . "<br>";
+        echo "Resta: " . calculadora($numeros[10]["numero"], $numeros[20]["numero"], $numeros[30]["resta"]) . "<br>";
+        echo "Multiplicacion: " . calculadora($numeros[10]["numero"], $numeros[20]["numero"], $numeros[30]["multiplicacion"]) . "<br>";
+        echo "Division: " . calculadora($numeros[10]["numero"], $numeros[20]["numero"], $numeros[30]["division"]) . "<br>";
+        echo "Resto: " . calculadora($numeros[10]["numero"], $numeros[20]["numero"], $numeros[30]["resto"]) . "<br>";
+        break;
+    };
+?>
