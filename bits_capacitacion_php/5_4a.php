c1efc156b7c70cf2997a161e36cde45d02408085
Nicholas Silva Ruiz <brandon.silva@bitsamericas.com>
2020-11-05 09:32:53 -0500

    Corrección ejercicio 5_4a.php

--- bits_capacitacion_php/5_4a.php
+++ bits_capacitacion_php/5_4a.php
@@ -27,34 +27,4 @@
 
         return $resultado;
     }
-
-    //--------------------------SUMA
-    $num1 = 80;
-    $num2 = 22;
-    $operacion = "+";
-    echo "El resultado de la Suma de $num1 + $num2 es : " . calculadora($num1, $num2, $operacion) . "<br><br>";
-
-    //-----------------------RESTA
-    $num1 = 40;
-    $num2 = 22;
-    $operacion = "-";
-    echo "El resultado de la Resta de $num1 - $num2 es : " . calculadora($num1, $num2, $operacion) . "<br><br>";
-
-    //-----------------------MULTIPLICACION
-    $num1 = 9;
-    $num2 = 9;
-    $operacion = "*";
-    echo "El resultado de la Multiplicacion de $num1 * $num2 es : " . calculadora($num1, $num2, $operacion) . "<br><br>";
-
-    //---------------------Division
-    $num1 = 50;
-    $num2 = 2;
-    $operacion = "/";
-    echo "El resultado de la Division de $num1 / $num2 es : " . calculadora($num1, $num2, $operacion) . "<br><br>";
-
-    //----------------------MOD
-    $num1 = 31;
-    $num2 = 18;
-    $operacion = "%";
-    echo "El Resto de  $num1 % $num2 es : " . calculadora($num1, $num2, $operacion) . "<br><br>";
 ?>
