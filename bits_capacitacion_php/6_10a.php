b7e6f7a22a69e0f6b9aa20b914b95958e0ab6e43
Nicholas Silva Ruiz <brandon.silva@bitsamericas.com>
2020-11-06 12:51:42 -0500

    Corrección ejercicio 6_10a.php

--- bits_capacitacion_php/6_10a.php
+++ bits_capacitacion_php/6_10a.php
@@ -4,10 +4,10 @@ $cadena = "1, 4, 7, 15, 222, 45, 68, 342";
 
 
 echo preg_replace_callback("/[0-9]{1,2}/", function ($coincidencia) { 
-    if($coincidencia[0] % 2 == 0) {
-        echo preg_replace("/[0-9]{1,2}/", $coincidencia[0] * 2 ,$coincidencia[0]) . "  El numero base se duplico <br><br>";
+    if($coincidencia[0] % 2 == 0){
+        echo $coincidencia[0] = $coincidencia[0] * 2 . " El numero base se multiplico por 2 <br>";
     }else {
-        echo preg_replace("/[0-9]{1,2}/", number_format(rand(1000, 10000), 2, ".", ","), $coincidencia[0]) . "  SUSTITUIDO <br><br>";
+        echo $coincidencia[0] = number_format(rand(1000, 10000), 2, '.', ',') . " Sustituido, numero base impar <br>";
     }
 }, $cadena);
 
