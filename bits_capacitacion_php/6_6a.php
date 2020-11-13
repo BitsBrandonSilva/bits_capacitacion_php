4afb1ca926f3ccd72ca18107a8df0d1722af3b94
Nicholas Silva Ruiz <brandon.silva@bitsamericas.com>
2020-11-12 20:57:48 -0500

    Solución ejercicio 6_6a.php

---
+++ bits_capacitacion_php/6_6a.php
@@ -0,0 +1,14 @@
+<?php
+
+    $users = [
+        3 => 'foo', 
+        24 => 'boB654', 
+        26 => 'billanderson', 
+        45 => 'paulmAc2'
+    ];
+   
+    $output = preg_grep("/\w{6}|[a-z]{1}|[A-Z]{1}|^[a-z]|[0-9]{1}/", $users);
+    
+    print_r($output);    
+
+?>
