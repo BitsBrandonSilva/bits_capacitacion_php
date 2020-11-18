2416772c6c947a649f12fb7403d5fe11b1da1258
Nicholas Silva Ruiz <brandon.silva@bitsamericas.com>
2020-11-18 18:40:08 -0500

    Formulario login

---
+++ bits_capacitacion_php/formularioLogin.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    
+    <form name = "formulario1" method = "post" action = "6_8a-process.php">
+      <label for="username">Nombre de usuario:</label>
+      <input type="text" id="username" name="username"><br><br>
+
+      <label for="password">Contraseña:</label>
+      <input type="password" id="password" name="password"><br><br>
+
+      <input type="submit" value="Submit">
+    </form>
+
+</body>
+</html>
