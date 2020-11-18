45cd07fdcf80ce9ccbc4917a2487c4c00bffa14e
Nicholas Silva Ruiz <brandon.silva@bitsamericas.com>
2020-11-18 18:39:40 -0500

    Solución 6_8a-login.php

---
+++ bits_capacitacion_php/6_8a-login.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sesion activa</title>
+</head>
+<body>
+    <?php
+       echo "El usuario " . $username = $_POST["username"] . " esta en sesion <br>";
+    ?>
+    
+    <a href = "6_8a-logout.php"> Cerrar sesion</a>
+</body>
+</html>
