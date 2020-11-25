41d7a0bb6c74a0e2e25a7d9a9779c2a786c89d0c
Nicholas Silva Ruiz <brandon.silva@bitsamericas.com>
2020-11-25 14:50:59 -0500

    Solución ejercicio 7_3c.php

--- bits_capacitacion_php/7_3c.php
+++ bits_capacitacion_php/7_3c.php
@@ -4,6 +4,17 @@
     use Php\Vehicles;
 
     class Truck extends Vehicles\Vehicle {
+
+        public $type = "Truck";
+
+        public function getType() {
+            return $this->type;
+        }
+
+        public function setType($type) {
+            $this->type = $type;
+        }
+
         public function slowDown($current_speed) {
             $current_speed = $current_speed - 0.5;
             return "La velociadad se ha decrementado " . $current_speed;
