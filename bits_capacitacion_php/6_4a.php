bd6d1b9da735e7a6e6f8b119c2a6bcfb6275c02a
Nicholas Silva Ruiz <brandon.silva@bitsamericas.com>
2020-12-01 18:14:04 -0500

    Corrección calidad 6_4a.php

--- bits_capacitacion_php/6_4a.php
+++ bits_capacitacion_php/6_4a.php
@@ -1,23 +1,36 @@
 <?php
 
-    //-----------------------------Punto 2 y 3
+    /**
+     * Punto 2 y 3
+     */
     $dates = [
-        ['day' => 3, 'month' => 12, 'year' => 2005, 'timestamp' => mktime(0, 0, 0, 12, 03, 2005)],
+        ['day' => 3, 'month' => 12, 'year' => 2005, 
+        'timestamp' => mktime(0, 0, 0, 12, 03, 2005)],
+
         ['day' => 30, 'month' => 2, 'year' => 2017, 'timestamp' => 'Error'],
-        ['day' => 7, 'month' => 9, 'year' => 2019, 'timestamp' => mktime(0, 0, 0, 9, 07, 2019)],
-        ['day' => 1, 'month' => 11, 'year' => 2014, 'timestamp' => mktime(0, 0, 0, 11, 01, 2014)],
-        ['day' => date("d"), 'month' => date("m"), 'year' => date("Y"), 'timestamp' => time()]
+
+        ['day' => 7, 'month' => 9, 'year' => 2019, 
+        'timestamp' => mktime(0, 0, 0, 9, 07, 2019)],
+
+        ['day' => 1, 'month' => 11, 'year' => 2014, 
+        'timestamp' => mktime(0, 0, 0, 11, 01, 2014)],
+
+        ['day' => date("d"), 'month' => date("m"), 'year' => date("Y"), 
+        'timestamp' => time()]
     ];
 
-    //-----------------------------Punto 1
+        /**
+         * Punto 1
+         */
     foreach ($dates as $fechas) {
         if (!checkdate($fechas['month'], $fechas['day'], $fechas['year'])) {
             unset($fechas['month'], $fechas['day'], $fechas['year']);
         }
-
     };
 
-    //-----------------------------Punto 4
+        /**
+         * Punto 4
+         */
     print_r($dates);
-    echo "<br><br>";
-?>
+
+    ?>
