504db31afabb0a155519f7a9f6e766e5903dd03c
Nicholas Silva Ruiz <brandon.silva@bitsamericas.com>
2020-12-01 22:37:15 -0500

    Corrección calidad clausulaBreak

--- bits_capacitacion_php/clausulaBreak.php
+++ bits_capacitacion_php/clausulaBreak.php
@@ -1,22 +1,24 @@
 <?php
 
+    /**
+     * Clausula break
+     */
     $vector = [22, 31, 40, 98];
 
-    $encontrado = FALSE;
+    $encontrado = false;
 
     $buscar = 98;
-    //$buscar = 15;
 
-    for ($i = 0; $i< count($vector); $i++) {
-        if ($vector[$i] == $buscar) {
-            $encontrado = TRUE;
-            break;
-        }
+for ($i = 0; $i< count($vector); $i++) {
+    if ($vector[$i] == $buscar) {
+        $encontrado = true;
+        break;
     }
+}
 
-    if ($encontrado) {
-        echo "Se ha encontrado el valor $buscar en la posicion $i";
-    } else {
-        echo "No se ha encontrado el valor $buscar";
-    }
+if ($encontrado) {
+    echo "Se ha encontrado el valor $buscar en la posicion $i";
+} else {
+    echo "No se ha encontrado el valor $buscar";
+}
 ?>
