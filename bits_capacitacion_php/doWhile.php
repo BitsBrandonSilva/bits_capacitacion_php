5088a98ea8f2fb5418bb8e3c9bcd54167607cdf5
Nicholas Silva Ruiz <brandon.silva@bitsamericas.com>
2020-12-02 15:20:12 -0500

    Corrección calidad doWhile

--- bits_capacitacion_php/doWhile.php
+++ bits_capacitacion_php/doWhile.php
@@ -1,10 +1,13 @@
 <?php
 
+    /**
+     * Estructura de repeticion doWhile
+     */
     echo "Bucle con Do While <br><br>";
 
     $i = 0;
-    do {
-        echo $i . "<br>";
-        $i++;
-    }while($i < 10);
+do {
+    echo $i . "<br>";
+    $i++;
+} while ($i < 10);
 ?>
