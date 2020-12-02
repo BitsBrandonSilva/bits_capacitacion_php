88976ee81d66c824ce77688c3da702f88a64204c
Nicholas Silva Ruiz <brandon.silva@bitsamericas.com>
2020-12-01 22:26:18 -0500

    Corrección calidad 6_3b.php

--- bits_capacitacion_php/6_3b.php
+++ bits_capacitacion_php/6_3b.php
@@ -1,5 +1,8 @@
 <?php
 
+    /**
+     * Ejercicio 6_3b
+     */
     $countries = [
     'uk' => 'United Kingdom',
     'fr' => 'France',
@@ -8,19 +11,19 @@
     'at' => 'Austria',
     'es' => 'Spain',
     'co' => 'Colombia'
-   ];
+    ];
 
-   //------------------Punto 1
-   $find = "Colombia";
-   if (in_array($find, $countries)) {
-       print "Mi pais de origen es " . $find . " y esta en la lista";
-   }else {
-    print "Mi pais de origen es " . $find . " pero no esta en la lista";
-   }
-   echo "<br><br>";
+    //------------------Punto 1
+    $find = "Colombia";
+    if (in_array($find, $countries)) {
+        print "Mi pais de origen es " . $find . " y esta en la lista";
+    } else {
+        print "Mi pais de origen es " . $find . " pero no esta en la lista";
+    }
+    echo "<br><br>";
 
-   //------------------Punto 2
-   asort($countries);
+    //------------------Punto 2
+    asort($countries);
     
     //------------------Punto 3
     print_r($countries); echo "<br><br>";
@@ -29,4 +32,4 @@
     $string = implode(", ", $countries);
     echo $string;
 
-?>
+    ?>
