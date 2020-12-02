6808fde9cebf8c00f1ad6c5afc41e3901b39414e
Nicholas Silva Ruiz <brandon.silva@bitsamericas.com>
2020-12-02 18:51:30 -0500

    Corrección calidad 6_10a.php

--- bits_capacitacion_php/6_10a.php
+++ bits_capacitacion_php/6_10a.php
@@ -1,15 +1,23 @@
 <?php
 
+    /**
+     * Ejercicio 6_10a
+     */
+
 $cadena = "1, 4, 7, 15, 222, 45, 68, 342";
 
 
-echo preg_replace_callback("/[0-9]{1,2}/", function ($coincidencia) { 
-    if($coincidencia[0] % 2 == 0){
-        echo $coincidencia[0] = $coincidencia[0] * 2 . " El numero base se multiplico por 2 <br>";
-    }else {
-        echo $coincidencia[0] = number_format(rand(1000, 10000), 2, '.', ',') . " Sustituido, numero base impar <br>";
-    }
-}, $cadena);
+echo preg_replace_callback(
+    "/[0-9]{1,2}/", function ($coincidencia) { 
+        if ($coincidencia[0] % 2 == 0) {
+            echo $coincidencia[0] = $coincidencia[0] * 2 
+            . " El numero base se multiplico por 2 <br>";
+        } else {
+            echo $coincidencia[0] = number_format(rand(1000, 10000), 2, '.', ',') 
+            . " Sustituido, numero base impar <br>";
+        }
+    }, $cadena
+);
 
 echo "<br><br>";
 
