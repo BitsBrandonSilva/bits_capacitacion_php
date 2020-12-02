d0fdc51f6a3704ba439dde38e26f02e5fbbb96ce
Nicholas Silva Ruiz <brandon.silva@bitsamericas.com>
2020-12-02 16:18:33 -0500

    Corrección calidad 5_4b.php

--- bits_capacitacion_php/5_4b.php
+++ bits_capacitacion_php/5_4b.php
@@ -1,6 +1,10 @@
 <?php
 
-    include '5_4a.php';
+    /**
+     * Ejercicio 5_4b
+     */
+
+    require '5_4a.php';
 
    $numeros = [
       10 => [
@@ -20,12 +24,22 @@
       ]
    ];
    
-    while ($numeros) {
-        echo "Suma: " . calculadora($numeros[10]["numero"], $numeros[20]["numero"], $numeros[30]["suma"]) . "<br>";
-        echo "Resta: " . calculadora($numeros[10]["numero"], $numeros[20]["numero"], $numeros[30]["resta"]) . "<br>";
-        echo "Multiplicacion: " . calculadora($numeros[10]["numero"], $numeros[20]["numero"], $numeros[30]["multiplicacion"]) . "<br>";
-        echo "Division: " . calculadora($numeros[10]["numero"], $numeros[20]["numero"], $numeros[30]["division"]) . "<br>";
-        echo "Resto: " . calculadora($numeros[10]["numero"], $numeros[20]["numero"], $numeros[30]["resto"]) . "<br>";
-        break;
-    };
-?>
+   while ($numeros) {
+       echo "Suma: " . calculadora(
+        $numeros[10]["numero"], $numeros[20]["numero"], 
+        $numeros[30]["suma"]) . "<br>";
+
+       echo "Resta: " . calculadora($numeros[10]["numero"], 
+       $numeros[20]["numero"], $numeros[30]["resta"]) . "<br>";
+
+       echo "Multiplicacion: " . calculadora($numeros[10]["numero"], 
+       $numeros[20]["numero"], $numeros[30]["multiplicacion"]) . "<br>";
+
+       echo "Division: " . calculadora($numeros[10]["numero"], 
+       $numeros[20]["numero"], $numeros[30]["division"]) . "<br>";
+
+       echo "Resto: " . calculadora($numeros[10]["numero"], 
+       $numeros[20]["numero"], $numeros[30]["resto"]) . "<br>";
+       break;
+   };
+    ?>
