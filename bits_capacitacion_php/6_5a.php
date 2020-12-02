e41e125287c8bd918590aa53c17d5a897ed63595
Nicholas Silva Ruiz <brandon.silva@bitsamericas.com>
2020-12-01 21:13:09 -0500

    Corrección calidad 6_5a.php

--- bits_capacitacion_php/6_5a.php
+++ bits_capacitacion_php/6_5a.php
@@ -1,13 +1,20 @@
 <?php
 
-    //-----------------Punto 1
+    /**
+     * Punto 1
+     */
     $fichero = 'php_example.log';
-    $data = $_SERVER['REMOTE_ADDR'] . " | " . time() . " | " . $_SERVER['HTTP_USER_AGENT'] . "\n";
+    $data = $_SERVER['REMOTE_ADDR'] . " | " . time() . 
+    " | " . $_SERVER['HTTP_USER_AGENT'] . "\n";
     
-    //-----------------Punto 2
+    /**
+     * Punto 2
+     */
     file_put_contents($fichero, $data, FILE_APPEND);
 
-    //-----------------Punto 3
+    /**
+     * Punto 3
+     */
     echo "Se ha alamacenado en el archivo " . $fichero . " la: " . $data;
 
 ?>
