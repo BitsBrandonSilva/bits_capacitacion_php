034c8fa164e9ec81a3f2a5b580c7427a8e17471d
Nicholas Silva Ruiz <brandon.silva@bitsamericas.com>
2020-12-02 16:33:42 -0500

    Corrección calidad 5_4a.php

--- bits_capacitacion_php/5_4a.php
+++ bits_capacitacion_php/5_4a.php
@@ -1,30 +1,31 @@
 <?php
 
-    function calculadora ($num1, $num2, $operacion) {
-        switch ($operacion) {
-            case '+' :
-                $resultado = $num1 + $num2;
-                break;
-
-            case '-' :
-                $resultado = $num1 - $num2;
-                break;
-
-            case '*' :
-                $resultado = $num1 * $num2;
-                break;
-
-            case '/' :
-                $resultado = $num1 / $num2;
-                break;
-
-            case '%' :
-                $resultado = $num1 % $num2;
-                break;
-            default:
-                echo "No es una operacion aritmetica";
-        }
-
-        return $resultado;
+/**
+ * Funcion calculadora
+ * 
+ * @return Operacion
+ */
+function calculadora($num1, $num2, $operacion)
+{
+    switch ($operacion) {
+    case '+' :
+        $resultado = $num1 + $num2;
+        break;
+    case '-' :
+        $resultado = $num1 - $num2;
+        break;
+    case '*' :
+        $resultado = $num1 * $num2;
+        break;
+    case '/' :
+        $resultado = $num1 / $num2;
+        break;
+    case '%' :
+        $resultado = $num1 % $num2;
+        break;
+    default:
+        echo "No es una operacion aritmetica";
     }
+    return $resultado;
+}
 ?>
