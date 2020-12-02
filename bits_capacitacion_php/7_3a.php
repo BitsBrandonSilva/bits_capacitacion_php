51f0a6291ccbaed45b493a6a3b3325a2c46100ec
Nicholas Silva Ruiz <brandon.silva@bitsamericas.com>
2020-12-02 18:37:17 -0500

    Corrección calidad 7_3a.php

--- bits_capacitacion_php/7_3a.php
+++ bits_capacitacion_php/7_3a.php
@@ -1,33 +1,61 @@
 <?php
 
-    require_once('7_1a.php');
+    require_once '7_1a.php';
     use Php\Vehicles;
 
-    class Motorbike extends Vehicles\Vehicle {
+    /**
+     * Clase que hereda de Vehicles
+     */
+class Motorbike extends Vehicles\Vehicle
+{
 
-        public $type = "Motorbike";
+    public $type = "Motorbike";
 
-        public function getType() {
-            return $this->type;
-        }
+    /**
+     * Fucion getType
+     * 
+     * @return type
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
 
-        public function setType($type) {
-            $this->type = $type;
+    /**
+     * Fucion setType
+     * 
+     * @return type
+     */
+    public function setType($type)
+    {
+        $this->type = $type;
+    }
+    
+    /**
+     * Funcio acceleration
+     * 
+     * @return acceleration
+     */
+    public function acceleration($current_speed, $fuel_level, $acceleration = false)
+    {
+        if ($acceleration > 0) {
+            $current_speed = $current_speed + $acceleration;
+            $fuel_level = $fuel_level - $acceleration;
+            return "El vehiculo " . $this->license_plate 
+            . " ha incrementado la velocidad en " 
+            . $acceleration . ". La velocidad acutal es " . $current_speed 
+            . ". El nivel de combustible es " . $fuel_level;
+        } else {
+            $current_speed = $current_speed + 3;
+            $fuel_level = $fuel_level - 3;
+            return "El vehiculo " . $this->license_plate 
+            . " ha incrementado la velocidad en " 
+            . $acceleration . ". La velocidad acutal es " . $current_speed 
+            . ". El nivel de combustible es " . $fuel_level;
         }
         
-        public function acceleration($current_speed, $fuel_level, $acceleration = false) {
-            if ($acceleration > 0) {
-                $current_speed = $current_speed + $acceleration;
-                $fuel_level = $fuel_level - $acceleration;
-                return "El vehiculo " . $this->license_plate . " ha incrementado la velocidad en " . $acceleration . ". La velocidad acutal es " . $current_speed . ". El nivel de combustible es " . $fuel_level;
-            } else {
-                $current_speed = $current_speed + 3;
-                $fuel_level = $fuel_level - 3;
-                return "El vehiculo " . $this->license_plate . " ha incrementado la velocidad en " . $acceleration . ". La velocidad acutal es " . $current_speed . ". El nivel de combustible es " . $fuel_level;
-            }
-            
-        }
-
     }
+
+}
 
 ?>
