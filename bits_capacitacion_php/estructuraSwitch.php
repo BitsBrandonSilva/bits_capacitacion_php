4da6a81953eaaa2114fd625944098de7cd64db28
Nicholas Silva Ruiz <brandon.silva@bitsamericas.com>
2020-12-01 22:47:53 -0500

    Corrección calidad estructura switch

--- bits_capacitacion_php/estructuraSwitch.php
+++ bits_capacitacion_php/estructuraSwitch.php
@@ -1,35 +1,36 @@
 <?php
 
+    /**
+     * Estructura Switch
+     */
     $day = 5;
 
-    switch($day) {
-        case 1: 
-            echo "Lunes";
-            break;
+switch($day) {
+case 1: 
+    echo "Lunes";
+    break;
+case 2:
+    echo "Martes";
+    break;
+case 3: 
+    echo "Miercoles";
+    break;
 
-        case 2:
-            echo "Martes";
-            break;
+case 4:
+    echo "Jueves";
+    break;
 
-        case 3: 
-            echo "Miercoles";
-            break;
-        
-        case 4:
-            echo "Jueves";
-            break;
-        
-        case 5:
-            echo "Viernes";
-            break;
-        case 6:
-            echo "Sabado";
-            break;
-        case 7:
-            echo "Domingo";
-            break;
-        default:
-            echo "Dia no valido";  
-    }
+case 5:
+    echo "Viernes";
+    break;
+case 6:
+    echo "Sabado";
+    break;
+case 7:
+    echo "Domingo";
+    break;
+default:
+    echo "Dia no valido";  
+}
 
 ?>
