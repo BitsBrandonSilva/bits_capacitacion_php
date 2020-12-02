37061ec20590e4122eea851b68d9b68901b8a03c
Nicholas Silva Ruiz <brandon.silva@bitsamericas.com>
2020-12-02 17:09:07 -0500

    Corrección calidad 5_3b.php

--- bits_capacitacion_php/5_3b.php
+++ bits_capacitacion_php/5_3b.php
@@ -1,9 +1,13 @@
 <?php
+
+  /**
+   * Ejercicio 5_3b
+   */
     $i = 1;
 
-    while ($i < 100) {
-      echo $i++ . " , ";
-    }
-    echo $i;
+while ($i < 100) {
+    echo $i++ . " , ";
+}
+  echo $i;
     
 ?>
