4be4ad8c9e573deeae46b78527543a17cade97cf
Nicholas Silva Ruiz <brandon.silva@bitsamericas.com>
2020-12-01 22:54:56 -0500

    Corrección calidad libreria

--- bits_capacitacion_php/libreria.php
+++ bits_capacitacion_php/libreria.php
@@ -1,12 +1,22 @@
 <?php
 
-    function incrementar_contador(&$contador, $valor_maximo = 5) {
-        if ($contador < $valor_maximo) {
-            $contador = $contador + 1;
-            return true;
-        } else {
-            return false;
-        }
+    /**
+     * Librerias
+     */
+
+    /**
+     * Funcion incrementar contador
+     * 
+     * @return contador++
+     */
+function Incrementar_contador(&$contador, $valor_maximo = 5) 
+{
+    if ($contador < $valor_maximo) {
+        $contador = $contador + 1;
+        return true;
+    } else {
+        return false;
     }
+}
 
 ?>
