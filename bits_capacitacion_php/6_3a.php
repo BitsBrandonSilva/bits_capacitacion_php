f083b81d4c7d49795a9264dfe7ec28c97e6c8e50
Nicholas Silva Ruiz <brandon.silva@bitsamericas.com>
2020-12-01 22:43:46 -0500

    Corrección calidad 6_3a.php

--- bits_capacitacion_php/6_3a.php
+++ bits_capacitacion_php/6_3a.php
@@ -1,5 +1,8 @@
 <?php
 
+    /**
+     * Ejercicio 6_3a
+     */
     $numbers = [3, 45, 22, 12, 33, 23, 44, 1, 121];
     $cadena = "5:76:7:8";
     $longitud = count($numbers);
@@ -10,15 +13,15 @@
     echo "Numero de elementos en el array : " . count($numbers) . "<br><br>";
 
     //--------------Punto 2
-    for ($i = 0; $i < $longitud; $i++) {
-        if ($numbers[$i] % 2 == 0) {
-            $cont1 ++;
-            echo "El numero es par: ". $numbers[$i] . "<br>";
-        }else {
-            $cont2++;
-            echo "El numero es impar: ". $numbers[$i] . "<br>";
-        };
+for ($i = 0; $i < $longitud; $i++) {
+    if ($numbers[$i] % 2 == 0) {
+        $cont1 ++;
+        echo "El numero es par: ". $numbers[$i] . "<br>";
+    } else {
+        $cont2++;
+        echo "El numero es impar: ". $numbers[$i] . "<br>";
     };
+};
 
     echo "Hay " . $cont1 . " pares <br>";
     echo "Hay " . $cont2 . " impares <br><br>";
@@ -27,7 +30,7 @@
     sort($numbers);
 
     //--------------Punto 4
-    $morenumbers = explode(":" , $cadena);
+    $morenumbers = explode(":", $cadena);
     
     //--------------Punto 5
     $numbers = array_merge($numbers, $morenumbers);
