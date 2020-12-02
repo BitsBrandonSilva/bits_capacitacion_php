82bda6e21822bcff9953e3257a430d2d380d480c
Nicholas Silva Ruiz <brandon.silva@bitsamericas.com>
2020-12-02 18:45:21 -0500

    Corrección calidad bucleWhile

--- bits_capacitacion_php/bucleWhile.php
+++ bits_capacitacion_php/bucleWhile.php
@@ -1,5 +1,8 @@
 <?php
 
+    /**
+     * Estructura bucle While
+     */
     $vector = [40, 31, 22, 10, 5];
 
     $suma_total = 0;
@@ -7,18 +10,18 @@
     $v = 0;
 
     //----------------- bucle while que muestra el vector
-    while($v < count($vector)) {
-        echo $vector[$v] . "<br>";
-        $v++;
-    }
+while ($v < count($vector)) {
+    echo $vector[$v] . "<br>";
+    $v++;
+}
     echo "<br>";
     $i = 0;
 
     //--------------------- bucle while que cuenta y suma el vector
-    while ($i < count($vector)) {
-        $suma_total = $suma_total + $vector[$i];
-        $i++;
-    }
+while ($i < count($vector)) {
+    $suma_total = $suma_total + $vector[$i];
+    $i++;
+}
 
     echo "La suma total del vector es: " . $suma_total;
 ?>
