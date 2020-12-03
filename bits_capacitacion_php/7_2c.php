a64ef7c6d1fb2de98b6205df1461099d379d1ed3
Nicholas Silva Ruiz <brandon.silva@bitsamericas.com>
2020-12-03 14:02:44 -0500

    Corrección calidad 7_2c.php

--- bits_capacitacion_php/7_2c.php
+++ bits_capacitacion_php/7_2c.php
@@ -6,13 +6,7 @@
     require '7_1a.php';
     use Php\Vehicles as Driving;
 
-    $vehicle1 = new Driving\Vehicle();
-    $vehicle1->setBrand("Ford");
-    $vehicle1->setSeats(4);
-    $vehicle1->setLicensePlate("SOD38B");
-    $vehicle1->setFuelLevel(10);
-    $vehicle1->setCurrentSpeed(1);
-
+    $vehicle1 = new Driving\Vehicle("Ford", 4, "SOD38B", 10, 1);
     print "Marca " . $vehicle1->getBrand() . "<br>";
     print "Sillas: " . $vehicle1->getSeats() . "<br>";
     print "Matricula: " . $vehicle1->getLicensePlate() . "<br>";
@@ -22,33 +16,14 @@
     print $vehicle1->fillTank(10, 10, 10) . "<br>";
     print $vehicle1->startEngine($vehicle1->getState()) . "<br>";
 
-    print $vehicle1->accelerate(
-        $vehicle1->getCurrentSpeed(), $vehicle1->getFuelLevel()
-    ). "<br>";
+for ($i = $vehicle1->getCurrentSpeed(); $i < 6; $i++) {
+    print $vehicle1->accelerate($i) . "<br>";
+}
 
-    print $vehicle1->accelerate(
-        $vehicle1->getCurrentSpeed(), $vehicle1->getFuelLevel(), 2
-    ) . "<br>";
+for ($j = $i; $j >= 0; $j--) {
+    print $vehicle1->slowDown($j) . "<br>";
+}
 
-    print $vehicle1->accelerate(
-        $vehicle1->getCurrentSpeed(), $vehicle1->getFuelLevel(), 3
-    ) . "<br>";
-
-    print $vehicle1->accelerate(
-        $vehicle1->getCurrentSpeed(), $vehicle1->getFuelLevel(), 4
-    ) . "<br>";
-
-    print $vehicle1->accelerate(
-        $vehicle1->getCurrentSpeed(), $vehicle1->getFuelLevel(), 5
-    ) . "<br>";
-
-    print $vehicle1->slowDown(6) . "<br>";
-    print $vehicle1->slowDown(5) . "<br>";
-    print $vehicle1->slowDown(4) . "<br>";
-    print $vehicle1->slowDown(3) . "<br>";
-    print $vehicle1->slowDown(2) . "<br>";
-    print $vehicle1->slowDown(1) . " ";
-    print $vehicle1->slowDown(0) . "<br>";
     print $vehicle1->stopEngine(0, 'On');
 
-    ?>
+?>
