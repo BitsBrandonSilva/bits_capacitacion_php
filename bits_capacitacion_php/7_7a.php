24618e9ae6199245cc267ff563e62a9913e8d802
Nicholas Silva Ruiz <brandon.silva@bitsamericas.com>
2020-12-02 19:39:30 -0500

    Corrección calidad 7_7a.php

--- bits_capacitacion_php/7_7a.php
+++ bits_capacitacion_php/7_7a.php
@@ -1,27 +1,38 @@
 <?php
 
-    include('7_6a.php');
+    require '7_6a.php';
 
-    class Cat {
-        use Insurance;
-    }
+    /**
+     * Clase Cat
+     */
+class Cat
+{
+    use Insurance;
+}
 
     $cat1 = new Cat();
     $cat1->setAge(2);
     $cat1->setName("Nikki");
     $cat1->calculateMontlyFee(2, "Nikki");
 
-    function catEat($eatCat){
-        if ($eatCat > 10) {
-            throw new Exception (' Ya esta bien alimentado <br>');
-        }
+    /**
+     * Fucion catEat
+     * 
+     * @return exception
+     */
+function catEat($eatCat)
+{
+    if ($eatCat > 10) {
+        throw new Exception(' Ya esta bien alimentado <br>');
     }
+}
 
-    for ($i = 0; $i < 12; $i++) {
-        try {
-            echo $cat1->getName() . "  puede seguir comiendo <br>" . catEat($i);
-        } catch (Exception $e) {
-            echo 'Mensaje del error: '. $cat1->getName() . $e->getMessage() . "<br>";
-        }
+for ($i = 0; $i < 12; $i++) {
+    try {
+        echo $cat1->getName() . "  puede seguir comiendo <br>" . catEat($i);
+    } catch (Exception $e) {
+        echo 'Mensaje del error: '. $cat1->getName() . $e->getMessage() . "<br>";
     }
+}
+
 ?>
