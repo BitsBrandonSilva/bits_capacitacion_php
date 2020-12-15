cdf33486b8b3cda8fb7b793ba546c8ea7ade80d1
Nicholas Silva Ruiz <brandon.silva@bitsamericas.com>
2020-12-14 20:26:15 -0500

    Corrección ejercicio 7_3a.php

--- bits_capacitacion_php/7_3a.php
+++ bits_capacitacion_php/7_3a.php
@@ -8,27 +8,18 @@
      */
 class Motorbike extends Vehicles\Vehicle
 {
-
-    public $type = "Motorbike";
-
     /**
-     * Fucion getType
+     * Funcion contructor Motorbike
      * 
      * @return type
      */
-    public function getType()
-    {
-        return $this->type;
-    }
-
-    /**
-     * Fucion setType
-     * 
-     * @return type
-     */
-    public function setType($type)
-    {
-        $this->type = $type;
+    public function __construc(
+        $brand, $seats, $license_plate, $fuel_capacity, $fuel_level, $current_speed
+        ) {
+        parent::__construc(
+            $brand, $seats, $license_plate, $fuel_capacity, $fuel_level, $current_speed
+        );
+        $this->type = 'motorobike';
     }
     
     /**
@@ -36,23 +27,10 @@ class Motorbike extends Vehicles\Vehicle
      * 
      * @return acceleration
      */
-    public function acceleration($current_speed, $fuel_level, $acceleration = false)
+    public function acceleration($acceleration = 1)
     {
-        if ($acceleration > 0) {
-            $current_speed = $current_speed + $acceleration;
-            $fuel_level = $fuel_level - $acceleration;
-            return "El vehiculo " . $this->license_plate 
-            . " ha incrementado la velocidad en " 
-            . $acceleration . ". La velocidad acutal es " . $current_speed 
-            . ". El nivel de combustible es " . $fuel_level;
-        } else {
-            $current_speed = $current_speed + 3;
-            $fuel_level = $fuel_level - 3;
-            return "El vehiculo " . $this->license_plate 
-            . " ha incrementado la velocidad en " 
-            . $acceleration . ". La velocidad acutal es " . $current_speed 
-            . ". El nivel de combustible es " . $fuel_level;
-        }
+        $acceleration = 3;
+        parent::acceleration($acceleration);
         
     }
 
