3c3442e3036a7cb3e80e13a3a40dbdd53da7bb87
Nicholas Silva Ruiz <brandon.silva@bitsamericas.com>
2020-12-14 20:25:56 -0500

    Corrección ejercicio 7_2c.php

--- bits_capacitacion_php/7_2c.php
+++ bits_capacitacion_php/7_2c.php
@@ -6,24 +6,30 @@
     require '7_1a.php';
     use Php\Vehicles as Driving;
 
-    $vehicle1 = new Driving\Vehicle("Ford", 4, "SOD38B", 10, 1);
+    $vehicle1 = new Driving\Vehicle("Ford", 4, "SOD38B", 10, 5, 1);
+    $vehicle1->setFuelCapacity(10);
     print "Marca " . $vehicle1->getBrand() . "<br>";
     print "Sillas: " . $vehicle1->getSeats() . "<br>";
     print "Matricula: " . $vehicle1->getLicensePlate() . "<br>";
     print "Velocidad actual: " . $vehicle1->getCurrentSpeed() . "<br>";
     print "Estado del coche: " . $vehicle1->getState() . "<br>";
 
-    print $vehicle1->fillTank(10, 10, 10) . "<br>";
-    print $vehicle1->startEngine($vehicle1->getState()) . "<br>";
+    print $vehicle1->fillTank(
+        $vehicle1->getFuelCapacity(), $vehicle1->getFuelLevel(), 2
+    ) . "<br>";
 
-for ($i = $vehicle1->getCurrentSpeed(); $i < 6; $i++) {
-    print $vehicle1->accelerate($i) . "<br>";
-}
+    print $vehicle1->startEngine() . "<br>";
 
-for ($j = $i; $j >= 0; $j--) {
-    print $vehicle1->slowDown($j) . "<br>";
-}
+    $longAccelerate = 6;
+    for ($i = 1; $i < $longAccelerate; $i++) {
+        print $vehicle1->accelerate() . "<br>";
+    }
 
-    print $vehicle1->stopEngine(0, 'On');
+    $longSlowDown = 6;
+    for ($i = 0; $i < $longSlowDown; $i++) {
+        print $vehicle1->slowDown() . "<br>";
+    }
+
+    print $vehicle1->stopEngine();
 
 ?>
